44fb3f5dd37c6e534e41c97423ce04526c0650e1
sofia tofigh <sst2140@columbia.edu>
2020-03-31 16:10:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,10 @@ def adding_matz
         :known_for => "Unix",
         :languages => ["C"]
       },
-      :
+      :yukihiro_matsumoto => {
+        :known_for => "Ruby"
+        :languages => ["LISP", "C"]
+      }
     }
 
 end
