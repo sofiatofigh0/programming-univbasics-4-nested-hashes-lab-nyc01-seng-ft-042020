796e3152d7c25a5fc93baad75940e118bf1dcf20
sofia tofigh <sst2140@columbia.edu>
2020-03-31 16:08:18 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,11 +15,13 @@ def hopper
         :languages => ["C"]
       }
     }
+    
+programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	
 	
 	programmer_hash = 
  		{
@@ -36,7 +38,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    
+programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
